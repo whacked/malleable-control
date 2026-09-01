5738623192550be9e997c5a723042cc54b41ff74
whacked <whacked@users.noreply.github.com>
2026-09-01 16:39:15 +0800

    Add coordinate-addressable corkboard prototype

--- test/run-pharo-tests.sh
+++ test/run-pharo-tests.sh
@@ -27,6 +27,9 @@ out="$("$GT_CLI" "$GT_IMAGE" eval "
   '$MC_HOME/pharo/McMarkdownReconciler.st' asFileReference fileIn.
   '$MC_HOME/pharo/McRichEdit.st' asFileReference fileIn.
   '$MC_HOME/pharo/McRichEditLinks.st' asFileReference fileIn.
+	'$MC_HOME/pharo/McCorkboardPanelModel.st' asFileReference fileIn.
+	'$MC_HOME/pharo/McCorkboardDocument.st' asFileReference fileIn.
+	'$MC_HOME/pharo/McCorkboard.st' asFileReference fileIn.
   '$MC_HOME/pharo/McRelationTest.st' asFileReference fileIn.
   '$MC_HOME/pharo/McCacheTest.st' asFileReference fileIn.
   '$MC_HOME/pharo/McMarkdownTest.st' asFileReference fileIn.
@@ -34,10 +37,11 @@ out="$("$GT_CLI" "$GT_IMAGE" eval "
   '$MC_HOME/pharo/McMarkdownReconcilerTest.st' asFileReference fileIn.
   '$MC_HOME/pharo/McRichEditPerfTest.st' asFileReference fileIn.
   '$MC_HOME/pharo/McMarkdownLinkTest.st' asFileReference fileIn.
+	'$MC_HOME/pharo/McCorkboardTest.st' asFileReference fileIn.
   (Smalltalk at: #McGtPatches) apply.
   failed := false.
   suites := OrderedCollection new.
-  #( #NatsClientTest #McRelationTest #McCacheTest #McMarkdownTest #McMarkdownSnapshotTest #McMarkdownReconcilerTest #McRichEditPerfTest #McMarkdownLinkTest ) do: [ :each |
+  #( #NatsClientTest #McRelationTest #McCacheTest #McMarkdownTest #McMarkdownSnapshotTest #McMarkdownReconcilerTest #McRichEditPerfTest #McMarkdownLinkTest #McCorkboardTest ) do: [ :each |
     result := (Smalltalk at: each) suite run.
     (result hasFailures or: [ result hasErrors ]) ifTrue: [ failed := true ].
     suites add: each -> result ].
